39b568684ee696a3152243a7e6b21771957da895
mhpranta12 <mhpranta2@gmail.com>
2025-03-21 16:33:20 +0600

    Added git ignore file for project

---
+++ SmartBudgetManagerUI/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        
+      </div>
+      <h1>Budget Mnagement System</h1>
+      <div className="card">
+      </div>
+      
+    </>
+  )
+}
+
+export default App
